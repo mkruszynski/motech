bdbf8ff8eccf575f8b267c8f6a589346c2e216b2
mkruszynski <mkruszynski@soldevelo.com>
2016-08-31 09:49:30 +0200

    MOTECH-2869: changed repo address3

--- after_failure.sh
+++ after_failure.sh
@@ -5,10 +5,10 @@ body='{
   "branch":"master"
 }}'
 
-curl -s -X POST
-  -H "Content-Type: application/json"
-  -H "Accept: application/json"
-  -H "Travis-API-Version: 3"
-  -H "Authorization: token 6aBhjLXTPBX3e_oszyrNzw"
-  -d "$body"
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token 6aBhjLXTPBX3e_oszyrNzw" \
+  -d "$body" \
   https://api.travis-ci.org/repo/mkruszynski%2Fmotech/requests
