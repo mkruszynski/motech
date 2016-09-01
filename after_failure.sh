510f4ed9f7e3ff0324a98fd0f54c0bbc032149b6
mkruszynski <mkruszynski@soldevelo.com>
2016-09-01 10:40:40 +0200

    MOTECH-2869: Test5

--- after_failure.sh
+++ after_failure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[$TRAVIS_BRANCH == 'master' ]]; then
-    mvn clean install -PIT
-elif [[$TRAVIS_PULL_REQUEST == true ]]; then
-    echo "PULL REQUEST"
+    mvn clean install -PIT;
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "PULL REQUEST";
 fi
