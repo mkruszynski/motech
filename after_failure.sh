29f7c38157fc7ad820605d57e259a2bafec882cf
mkruszynski <mkruszynski@soldevelo.com>
2016-09-01 10:05:27 +0200

    MOTECH-2869: test3

--- after_failure.sh
+++ after_failure.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-#body='{
-#"request": {
-#  "branch":"master"
-#}}'
-
 if [[$TRAVIS_BRANCH == 'master' ]]; then
     mvn clean install -PIT;
 elif [[$TRAVIS_PULL_REQUEST == true ]]; then
     echo "PULL REQUEST";
 fi
-#curl -s -X POST \
-# -H "Content-Type: application/json" \
-#  -H "Accept: application/json" \
-#  -H "Travis-API-Version: 3" \
-#  -H "Authorization: token 6aBhjLXTPBX3e_oszyrNzw" \
-#  -d "$body" \
-#  https://api.travis-ci.org/repo/mkruszynski%2Fmotech/requests
