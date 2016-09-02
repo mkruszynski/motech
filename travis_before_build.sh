2f186561239687cac978ae9951f28550bf5d5b54
mkruszynski <mkruszynski@soldevelo.com>
2016-09-02 13:26:12 +0200

    MOTECH-2865: Try to run postgres test v1

--- travis_before_build.sh
+++ travis_before_build.sh
@@ -3,7 +3,7 @@
 if [ "$DB" = "mysql" ]; then
     echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
 elif [ "$DB" = "psql" ]; then
-    ls ~/
-    sed -i 's/motech.sql.dbtype=mysql/motech.sql.dbtype=psql/g' ~/motech/maven.properties
-    cat ~/motech/maven.properties
+    ls /
+    sed -i 's/motech.sql.dbtype=mysql/motech.sql.dbtype=psql/g' /motech/maven.properties
+    cat /motech/maven.properties
 fi
