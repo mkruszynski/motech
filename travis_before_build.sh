53d53a40c3d752cd8ed8d88f5675beb8923f21bf
mkruszynski <mkruszynski@soldevelo.com>
2016-09-02 11:51:35 +0200

    MOTECH-2865: tests on 2 bases

---
+++ travis_before_build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ "$DB" = "mysql" ]; then
+    echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
+elif [ "$DB" = "psql" ]; then
+    sudo apt-get install postgresql
+    sed -i 's/motech.sql.dbtype=mysql/motech.sql.dbtype=psql/g' ~/motech/maven.properties
+    cat ~/motech/maven.properties
+fi
