a5e0c7d2942203d31499de8af79d77107cfbc430
mkruszynski <mkruszynski@soldevelo.com>
2016-09-14 08:27:56 +0200

    Motech 2922 test (#8)
    * Use node-sass v3.8.0 (#683)
    
    * Build fix
    
    * Build fix #2
    
    * Build fix
    
    * MOTECH-2899 Gap between panels in Motech homepage (#687)
    
    * MOTECH-2884 Extend Date Manipulation Functions in Tasks (#680)
    
    * MOTECH-2884
    
    * MOTECH-2884 Extend Date Manipulation Functions in Tasks
    
    * MOTECH-2884 Extend Date Manipulation Functions in Tasks
    
    * MOTECH-2884 Extend Date Manipulation Functions in Tasks
    
    * MOTECH-2912 fixed event module menu item (#689)
    
    * MOTECH-2897: Fixed lacking translations
    
    * MOTECH-2514 Button "clear" should be blocked (#555)
    
    * MOTECH-2514 - fixed the issue where clear button is enable even if nothing is selected
    
    * removing unnecessary checks
    
    * MOTECH-2897: Fixed after review
    
    * MOTECH-2897: Fixed lacking translations
    
    Now translations are good for all languages.
    
    * MOTECH-2897: Fixed after review
    
    * First commit

---
+++ config_services.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -qq
+
+#Installation and configuration of ActiveMQ
+sudo apt-get install -y activemq -qq
+sudo ln -s /etc/activemq/instances-available/main /etc/activemq/instances-enabled/main
+sudo sed -e 's/<broker /<broker schedulerSupport="true" /' -i /etc/activemq/instances-enabled/main/activemq.xml
+sudo service activemq restart
+
+#Install and configure Tomcat, only if IT's for MOTECH will be fire
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    #Installation and configuration of Tomcat
+    sudo apt-get install -y tomcat7 -qq
+    sudo service tomcat7 stop
+    echo 'CATALINA_OPTS="-Xms512m -Xmx512m"' | sudo tee --append /usr/share/tomcat7/bin/setenv.sh
+    sudo chown -R tomcat7:tomcat7 /var/lib/tomcat7/ /usr/share/tomcat7/ /var/log/tomcat7/
+fi
+
+if [ "$DB" = "mysql" ]; then
+    echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
+fi
---
+++ run_build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    if [ "$DB" = "psql" ]; then
+        mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U
+    else
+        mvn clean install -PIT -U
+    fi
+elif [ "$TRAVIS_BRANCH" = "master" ] && [ "$DB" = "mysql" ]; then
+    MOTECH_LOCATION = `pwd`
+    git config user.email "travis-test-maniek@googlegroups.com"
+
+    #Download and test Modules
+    git clone https://github.com/motech/modules.git ../modules -b master --single-branch
+    cd ../modules/
+    mvn clean install -PIT -U
+
+    #If build fails, return error code
+    if [ "$?" -ne 0 ]; then
+        exit 1
+    fi
+
+    #Deploy MOTECH
+    cd $MOTECH_LOCATION
+    mvn -Dmaven.test.skip=true clean deploy -U
+fi
