71c36a1e2195e82e6e7bd75d4908e030f0c1fd07
mkruszynski <mkruszynski@soldevelo.com>
2016-09-16 10:14:55 +0200

    3rd try

--- config_services.sh
+++ config_services.sh
@@ -7,3 +7,6 @@ sudo apt-get install -y activemq -qq
 sudo ln -s /etc/activemq/instances-available/main /etc/activemq/instances-enabled/main
 sudo sed -e 's/<broker /<broker schedulerSupport="true" /' -i /etc/activemq/instances-enabled/main/activemq.xml
 sudo service activemq restart
+
+#Set mysql root password
+echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
-    echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
+    git clone https://github.com/motech/modules.git ../motech-master -b master --single-branch
+    cd ../motech-master/
     mvn -Dmysql.password=password -Dmaven.test.failure.ignore=false -Dmysql.user=root -DunitTests.skip=true clean install -PFT
 fi
