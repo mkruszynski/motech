440e6d97b4c60a9502b9ba316677198f78a1709f
mkruszynski <mkruszynski@soldevelo.com>
2016-09-16 13:05:42 +0200

    12th try

--- config_services.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-#Set mysql root password
-echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
--- run_build.sh
+++ run_build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
+    #Set mysql root password
+    echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
     git clone https://github.com/motech/motech.git ../motech-master -b master --single-branch
-    cd ../motech-master/
     mkdir ~/.motech
-    cp ./testdata/config-locations.properties ~/.motech/
-    cd ./modules/admin/
+    cp ../motech-master/testdata/config-locations.properties ~/.motech/
+    cd ../motech-master/modules/admin/
     mvn -Dmysql.password=password -Dmaven.test.failure.ignore=false -Dmysql.user=root -DunitTests.skip=true clean install -PFT -U
 fi
