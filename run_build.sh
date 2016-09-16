f5c545853200b070f87616ccdba581ab25dd05bc
mkruszynski <mkruszynski@soldevelo.com>
2016-09-16 12:58:01 +0200

    11th try

--- run_build.sh
+++ run_build.sh
@@ -4,7 +4,6 @@ if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
     git clone https://github.com/motech/motech.git ../motech-master -b master --single-branch
     cd ../motech-master/
     mkdir ~/.motech
-    mkdir ~/.motech/config
     cp ./testdata/config-locations.properties ~/.motech/
     cd ./modules/admin/
     mvn -Dmysql.password=password -Dmaven.test.failure.ignore=false -Dmysql.user=root -DunitTests.skip=true clean install -PFT -U
