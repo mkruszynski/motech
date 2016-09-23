01e4338e163c6434990bab65ee314bf1ff11fc5b
mkruszynski <mkruszynski@soldevelo.com>
2016-09-23 14:55:02 +0200

    MOTECH-2922: parallel test 2

--- run_build.sh
+++ run_build.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$DB" = "psql" ]; then
         mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U
-    else
+    elif [ "$DB" = "psql" ] && [ "$REPOSITORY" = "motech" ]; then
         mvn clean install -PIT -U
     fi
 elif [ "$TRAVIS_BRANCH" = "master" ] && [ "$DB" = "mysql" ]; then
