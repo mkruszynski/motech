d016b73674ea5eb1a29d06a38c8341f1f154c0ca
mkruszynski <mkruszynski@soldevelo.com>
2016-11-15 11:58:07 +0100

    Travis release v13

--- run_build.sh
+++ run_build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #Release
-if [ "$TRAVIS_EVENT_TYPE" = "api" ] && [ ! -z "$developmentVersion" ] && [ ! -z "$scmTag" ] && [ ! -z "$releaseVersion" ]; then
+if [ "$TRAVIS_EVENT_TYPE" = "api" ] && [ ! -z "$developmentVersion" ] && [ ! -z "$scmTag" ] && [ ! -z "$releaseVersion" ] && [ ! -z "$githubMail" ] && [ ! -z "$githubUsername" ]; then
     #mvn --settings deploy-settings.xml clean deploy -e -PIT,DEB,RPM -B -U
-    git config --global user.email "$developmentVersion"
-    git config --global user.name "$developmentVersion"
+    git config --global user.email "$githubMail"
+    git config --global user.name "$githubUsername"
     mvn -DdevelopmentVersion=$developmentVersion -Dscm.tag=$scmTag -DreleaseVersion=$releaseVersion -Dmaven.test.failure.ignore=false -Dscm.developerConnection=scm:git:git@github.com:mkruszynski/motech.git -Dscm.connection=scm:git:git@github.com:mkruszynski/motech.git release:clean release:prepare release:perform
 fi
 
