f48a95a45947ecc588e2a216922e8d2e419b204a
mkruszynski <mkruszynski@soldevelo.com>
2016-11-15 13:41:34 +0100

    Travis release v14

--- run_build.sh
+++ run_build.sh
@@ -3,8 +3,13 @@
 #Release
 if [ "$TRAVIS_EVENT_TYPE" = "api" ] && [ ! -z "$developmentVersion" ] && [ ! -z "$scmTag" ] && [ ! -z "$releaseVersion" ] && [ ! -z "$githubMail" ] && [ ! -z "$githubUsername" ]; then
     #mvn --settings deploy-settings.xml clean deploy -e -PIT,DEB,RPM -B -U
+    
     git config --global user.email "$githubMail"
     git config --global user.name "$githubUsername"
+    git clean -fdx
+    git checkout -f $TRAVIS_BRANCH
+    git reset --hard $TRAVIS_BRANCH
+
     mvn -DdevelopmentVersion=$developmentVersion -Dscm.tag=$scmTag -DreleaseVersion=$releaseVersion -Dmaven.test.failure.ignore=false -Dscm.developerConnection=scm:git:git@github.com:mkruszynski/motech.git -Dscm.connection=scm:git:git@github.com:mkruszynski/motech.git release:clean release:prepare release:perform
 fi
 
